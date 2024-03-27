2d9c2344616ee992d818989b09c3da21e0cf5cc1
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-03-27 10:39:02 -0400

    Add user choice prompt

--- rps.js
+++ rps.js
@@ -5,3 +5,8 @@ function getComputerChoice() {
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
+
+function getPlayerChoice() {
+  const playerChoice = prompt("Enter your choice: rock|paper|scissors ->: ").toLowerCase();
+  return playerChoice;
+}
