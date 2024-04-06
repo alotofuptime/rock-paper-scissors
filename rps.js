1cb04cd7574e09903e3cca22f25417098f497411
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-04-06 11:02:47 -0400

    Create initial UI skeleton

--- rps.js
+++ rps.js
@@ -1,31 +1,34 @@
-"use strict";
+'use strict';
 
-playGame(5);
+// playGame(5);
 
 function getComputerChoice() {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = ['rock', 'paper', 'scissors'];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
 function getPlayerChoice() {
-  const playerChoice = prompt("Enter your choice: rock | paper | scissors ->: ").toLowerCase();
+  const playerChoice = prompt(
+    'Enter your choice: rock | paper | scissors ->: '
+  ).toLowerCase();
   return playerChoice;
 }
 
-function playRound(cpuSelection, plyrSelection) {
-  const tie = cpuSelection === plyrSelection;
-  const playerWins = !tie && (
-    (plyrSelection === "rock") && (cpuSelection !== "paper")
-    || (plyrSelection === "paper") && (cpuSelection !== "scissors")
-    || (plyrSelection === "scissors") && (cpuSelection !== "rock")
-  );
+function playRound(cpuSelection, playerSelection) {
+  const tie = cpuSelection === playerSelection;
+  const playerWins =
+    !tie &&
+    ((playerSelection === 'rock' && cpuSelection !== 'paper') ||
+      (playerSelection === 'paper' && cpuSelection !== 'scissors') ||
+      (playerSelection === 'scissors' && cpuSelection !== 'rock'));
 
-
-  const roundResult = tie ? "It's a tie" : playerWins ? "You win" : "You lose";
-  const roundDetails = tie ? `You both chose ${cpuSelection}.`
-    : playerWins ? `${plyrSelection} beats ${cpuSelection}.`
-      : `${cpuSelection} beats ${plyrSelection}.`;
+  const roundResult = tie ? "It's a tie" : playerWins ? 'You win' : 'You lose';
+  const roundDetails = tie
+    ? `You both chose ${cpuSelection}.`
+    : playerWins
+    ? `${playerSelection} beats ${cpuSelection}.`
+    : `${cpuSelection} beats ${playerSelection}.`;
 
   const round = {
     playerWon: playerWins,
@@ -37,31 +40,31 @@ function playRound(cpuSelection, plyrSelection) {
   return round;
 }
 
-function playGame(rounds) {
-  let plyrScore = 0;
-  let cpuScore = 0;
-  let currentRd = 1;
-  let rd;
+// function playGame(rounds) {
+//   let playerScore = 0;
+//   let cpuScore = 0;
+//   let currentRd = 1;
+//   let rd;
 
-  while (currentRd <= rounds) {
-    rd = playRound(getComputerChoice(), getPlayerChoice());
+//   while (currentRd <= rounds) {
+//     rd = playRound(getComputerChoice(), getPlayerChoice());
 
-    if (rd.tie) {
-      plyrScore++;
-      cpuScore++;
-    } else if (rd.playerWon) {
-      plyrScore++;
-    } else cpuScore++;
+//     if (rd.tie) {
+//       playerScore++;
+//       cpuScore++;
+//     } else if (rd.playerWon) {
+//       playerScore++;
+//     } else cpuScore++;
 
-    let stats = `Round: ${currentRd} Player: ${plyrScore} CPU: ${cpuScore}`;
-    console.log(stats);
-    console.log(rd.message.concat("\n", rd.details));
-    currentRd++;
-  }
+//     let stats = `Round: ${currentRd} Player: ${playerScore} CPU: ${cpuScore}`;
+//     console.log(stats);
+//     console.log(rd.message.concat('\n', rd.details));
+//     currentRd++;
+//   }
 
-  if (plyrScore === cpuScore) {
-    console.log("It's a tie!");
-  } else if (plyrScore > cpuScore) {
-    console.log("You win!");
-  } else console.log("You lose :(");
-}
+//   if (playerScore === cpuScore) {
+//     console.log("It's a tie!");
+//   } else if (playerScore > cpuScore) {
+//     console.log('You win!');
+//   } else console.log('You lose :(');
+// }
