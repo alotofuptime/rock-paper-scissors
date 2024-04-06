d74acaab600189996c4f7618f4e45b0ceb330766
alotofuptime <77962905+alotofuptime@users.noreply.github.com>
2024-04-06 12:28:42 -0400

    Refactor part of the business logic from rps.js
    Having the available choices in the form an object instead of an
    array makes everything a lot cleaner.
    
    playerWins is a more readable portion of the playRound function in
    rps.js. The playRound function does too many things on its own and
    is extremely messy in terms of readablity.

--- app.js
+++ app.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const choices = {
+  rock: 'rock',
+  paper: 'paper',
+  scissors: 'scissors',
+};
+
+const getComputerChoice = () => {
+  const cpuChoices = Object.values(choices);
+  const randomIndex = Math.floor(Math.random() * cpuChoices.length);
+  return cpuChoices[randomIndex];
+};
+
+const playerWins = (cpuChoice, playerChoice) => {
+  const playerWinsWithRock = playerChoice === choices.rock && cpuChoice !== choices.paper;
+  const playerWinsWithPaper = playerChoice === choices.paper && cpuChoice !== choices.scissors;
+  const playerWinsWithScissors = playerChoice === choices.scissors && cpuChoice !== choices.rock;
+  return playerWinsWithRock || playerWinsWithPaper || playerWinsWithScissors;
+};
+
 const playerOptions = document.querySelector('.options');
 playerOptions.addEventListener('click', (e) => {
   const playerChoice = e.target.classList.value;
